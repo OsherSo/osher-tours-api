ef4ff9ce7a67269384aae10e4095098e989b5af7
Osher Solimany <solimani.osher@gmail.com>
2023-05-07 11:24:56 +0300

    Create reviewController.js

---
+++ controllers/reviewController.js
@@ -0,0 +1,21 @@
+// Import dependencies
+const Review = require('../models/reviewModel');
+const factory = require('./handlerFactory');
+
+// Define functions to handle HTTP requests
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
+
+// Middleware to set tour and user IDs before creating a review
+exports.setTourUserIds = (req, res, next) => {
+  if (!req.body.tour) {
+    req.body.tour = req.params.tourId;
+  }
+  if (!req.body.user) {
+    req.body.user = req.user._id;
+  }
+  next();
+};
