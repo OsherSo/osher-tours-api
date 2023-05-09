a0cb9ceb02da46e2e8087405f95eba9b054051ce
Osher Solimany <solimani.osher@gmail.com>
2023-05-10 01:15:39 +0300

    Update handlerFactory.js

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -7,7 +7,10 @@ exports.getAll = (Model) =>
     let filter = {};
     if (req.params.tourId) filter = { tour: req.params.tourId };
 
-    const features = new APIFeatures(Model.find(filter), req.query)
+    const features = new APIFeatures(
+      Model.find(filter).populate('reviews'),
+      req.query
+    )
       .filter()
       .sort()
       .limitFields()
