c3daf69d67ba4a39c40fc12e97874f924933066b
Osher Solimany <solimani.osher@gmail.com>
2023-05-14 15:53:38 +0300

    Remove some functions

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -19,18 +19,6 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user.id);
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-
-  if (process.env.NODE_ENV === 'production') {
-    cookieOptions.secure = true;
-  }
-
-  res.cookie('jwt', token, cookieOptions);
   user.password = undefined;
 
   res.status(statusCode).json({
@@ -48,10 +36,6 @@ exports.signup = catchAsync(async (req, res, next) => {
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
-
-  if (!email || !password) {
-    return next(new AppError('Please provide email and password.', 400));
-  }
 
   const user = await User.findOne({ email }).select('+password');
 
